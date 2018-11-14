1334bd4bc3202e2399fdf19dff0ae5ebaed71d8d
CTlandry <clandry1982@gmail.com>
2018-11-13 22:14:07 -0700

    Cleaning Up UI

--- ContactsDemo/ContactsDemo/ViewModel/ViewModel_ContactDetail.cs
+++ ContactsDemo/ContactsDemo/ViewModel/ViewModel_ContactDetail.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ContactsDemo.ViewModel
 {
@@ -19,6 +22,40 @@ namespace ContactsDemo.ViewModel
             PropertyContact = contact;
         }
 
-       
+        private ICommand _PhoneCall;
+        public ICommand PhoneCall
+        {
+            get
+            {
+                return _PhoneCall ?? (_PhoneCall = new Command(async (phonenumber) => await ExecutePhoneCall((string)phonenumber)));
+            }
+        }
+
+        public async Task ExecutePhoneCall(string number)
+        {
+            await Task.Run(() =>
+            {
+                DependencyService.Get<IPhone>().PlaceCall(number);
+            });
+        }
+
+        private ICommand _SendEmail;
+        public ICommand SendEmail
+        {
+            get
+            {
+                return _SendEmail ?? (_SendEmail = new Command(async (toemail) => await ExecuteSendEmail((string)toemail)));
+            }
+        }
+
+        public async Task ExecuteSendEmail(string toEmail)
+        {
+            await Task.Run(() =>
+            {
+                DependencyService.Get<IEmail>().OpenEmailClient(toEmail);
+            });
+        }
+
+
     }
 }
